801d660d978affe5286dae12b2ed98c5a4714622
snandakumar87 <snandaku@redhat.com>
2020-10-09 06:49:06 -0400

    changed url for ansible

--- invoke-ansible/src/main/java/com/redhat/riskvalidationservice/routes/RiskValidationRouteBuilder.java
+++ invoke-ansible/src/main/java/com/redhat/riskvalidationservice/routes/RiskValidationRouteBuilder.java
@@ -47,7 +47,7 @@ public class RiskValidationRouteBuilder extends RouteBuilder {
 					+ "&groupId=" + consumerGroup).id("apbEvents")
 			.bean(RiskValidationBean.class,"prepareAnsibleRequest")
 					.setHeader(Exchange.HTTP_METHOD, constant("POST"))
-					.setHeader("Authorization",constant("Bearer xxxxxxxxxxx"))
+					.setHeader("Authorization",constant("Bearer nnZvQ54MgWxFgiDlczIxNDZqCCM0i0"))
 					.setHeader("Content-Type",constant("application/json"))
 					.toD("https4://"+ansibleTowerUrl+"/api/v2/job_templates/${header.apbName}/launch/")
 					.bean(RiskValidationBean.class,"readAnsibleResponse")
